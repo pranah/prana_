6a13d090cc7daf2eecfbff4b8ae55ed79a4a890a
mansoorop <opmansoor7@gmail.com>
2021-03-18 00:59:59 +0530

    form added

--- packages/react-app/src/tabs/Publish.js
+++ packages/react-app/src/tabs/Publish.js
@@ -1,11 +1,40 @@
-import React from 'react'
-import Button from '@material-ui/core/Button';
+import React from "react";
+import "./formstyles.css";
+
+import { Form, Field } from "@leveluptuts/fresh";
+import styled, { css } from 'styled-components';
+
+
+
+const onSubmit = (data) => console.log(data);
 
 export function Publish() {
   return (
-    <>
-    <h2>Publish Component</h2>
-    
-    </>
-  )
+    // <Container>
+    <Form formId="book-publish" onSubmit={onSubmit}>
+      <Field required >Title</Field>
+      <Field required type="number">ISBN</Field>
+      <Field required type="number">Price</Field>
+      <Field required type="number">Creator's cut</Field>
+      <label>Book cover</label><input type="file" />
+      
+      <label>Book content</label><input type="file" />
+    </Form>
+    // </Container>
+  );
 }
+
+// const Container = styled.div`
+// box-sizing: border-box;
+// background-color: lightblue;
+// padding: 50px;
+// `
+// const fresh-fo
+//   font-family: Arial;
+//   width: 400px;
+//   margin: 0 auto;
+//   background-color: white;
+//   padding: 2rem;
+//   box-shadow: 0px 2px 5px 0px rgba(0, 0, 0, 0.25);
+//   border-top: 5px solid steelblue;
+// }
